52b4c8ef61b9395537011e58eb29f8fa1b231871
KALIZEI611 <mih80852@gmail.com>
2025-11-04 20:11:58 +0300

    add

---
+++ src/CollectionAdapter/MenuManager.java
@@ -0,0 +1,16 @@
+package CollectionAdapter;
+
+public class MenuManager {
+
+    // Показать меню
+    public static void showMenu() {
+        System.out.println("\n=== МЕНЮ ===");
+        System.out.println("1. Добавить элемент в коллекцию");
+        System.out.println("2. Удалить элемент из коллекции");
+        System.out.println("3. Показать содержимое коллекции");
+        System.out.println("4. Проверить есть ли значение в коллекции");
+        System.out.println("5. Заменить значение в коллекции");
+        System.out.println("6. Показать тип коллекции");
+        System.out.println("0. Выход");
+    }
+}
